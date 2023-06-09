54cfb0e39eeca6aaed40fb04a55a551d8d207eb0
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-06-09 13:35:29 -0400

    feat: add kubectl events plugin
    Function that prints out kubernetes events in a usable format

---
+++ functions/kubectl-events.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+function kubectl-events -d "view kubernetes events by timestamp" --wraps kubectl
+    begin
+        echo -e "TIME\tNAMESPACE\tTYPE\tREASON\tOBJECT\tSOURCE\tMESSAGE"
+        kubectl get events -o json $argv \
+            | jq -r '.items | map(. + {t: (.eventTime//.lastTimestamp)}) | sort_by(.t)[] | [.t, .metadata.namespace, .type, .reason, .involvedObject.kind + "/" + .involvedObject.name, .source.component + "," + (.source.host//"-"), .message] | @tsv'
+    end | column -t -s "$(printf '\t')" | less -S
+end
